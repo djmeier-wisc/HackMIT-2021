0391ecffb612779842dd22b8bdc626f449c22da7
djmeier-wisc <71796515+djmeier-wisc@users.noreply.github.com>
2021-09-18 18:16:11 -0500

    init

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,13 @@
-function About () {
-    return (
-        <div className='AboutUs'>
-            <h1>A</h1>
+class About extends React.Component {
+    render() {
+        return (
+        <div className='container'>
+            <h1>About Us</h1>
+            <br />
+            <p>Welcome to <b>Brokers!</b> We have worked hard to connect college students the information they need to finance higher eduction.</p>
         </div>
-    );
+        );
+    }
 }
 
 export default About;
