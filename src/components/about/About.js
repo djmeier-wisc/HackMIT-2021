1c650ec7f030c5e14379239c37822ed16f9df2a2
djmeier-wisc <71796515+djmeier-wisc@users.noreply.github.com>
2021-09-19 09:43:45 -0500

    added description and new homepage

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -27,7 +27,7 @@ function About () {
             <div className="container">
                 <div className="row">
                     <div className="col-md-3">
-                        <ContactCard image={nicImg} name='Nic Hodlofski' description='' />
+                        <ContactCard image={nicImg} name='Nic Hodlofski' description="" />
                     </div>
                     <div className="col-md-3">
                         <ContactCard image={harryImg} name='Harry Wang' description='I am Harry! I am currently a Junior at UW-Madison, studying Computer Science and Mathematics. I am interested in back-end programming. I like reading fiction during my free time, and sometimes I would go for a run. And when I am not doing these things, I am usually sleeping….' />
@@ -36,7 +36,7 @@ function About () {
                         <ContactCard image={dougImg} name='Douglas Meier' description="Great to meet you! I've mostly worked on projects with ASP.NET MVC 5, but am excited to learn more about react. I study Computer Sciences and French at UW-Madison, graduating in May 2022" />
                     </div>
                     <div className="col-md-3">
-                        <ContactCard image={brettImg} name='Brett Falck' description='' />
+                        <ContactCard image={brettImg} name='Brett Falck' description="Hey everyone, I'm Brett Falck a Junior at UW - Madison in Computer Science. In my free time I enjoy running, biking, reading, and of course programming." />
                     </div>
                 </div>
             </div>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,7 @@ function Home () {
             <h1 className="pb-5">Home</h1>
             <div className='row'>
                 <div className='col-md-6'>
-                    <p>Our app, Brokers, is intended for College/High school students. 'Brokers' allows a student to track their financial balance using our state of the art algorithms. The personalized algorithms enable users to track and log their previous financial logs and project future expenses.  The secondary usage of the app extends beyond the traditional expense tracking app and venture into scholarship assistance. Under the "Scholarship" tab, our web scraping algorithm uses the user permitted info and gathers relevant scholarships that the user might qualify for. The "Scholarship" section aims to promote better financial awareness among students, to encourage seek out many means of income to achieve higher education. The final phase of the app is to integrate the Scholarship feature with the financial section and allows the user to see their current and projected financial balance, given different scholarship possibilities. We aim to promote financial awareness in higher education through technology, alleviating the stress for many college students.
+                    <p>Our app, Brokers, is intended for College/High school students. ‘Brokers’ is a React JS web application that helps students find more scholarships to finance their education by pulling scholarships from multiple sources using web scraping tools like Scrapy. The links to all scholarships are provided on the Scholarship page.
                     </p>
                 </div>
                 <div className='col-md-6'>
